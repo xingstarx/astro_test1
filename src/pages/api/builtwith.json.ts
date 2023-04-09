e020aa11a44808534ee2a925ce49cfab31cf8bc6
xingxing <823952375@qq.com>
2023-04-09 21:09:08 +0800

    获取ip

--- src/pages/api/builtwith.json.ts
+++ src/pages/api/builtwith.json.ts
@@ -1,10 +1,19 @@
 //静态文件端点
 // 输出: /api/builtwith.json
+
+import RequestIp from '@supercharge/request-ip';
+
 export async function get({params, request}) {
+    // const ip = request.ip; // 获取请求的IP地址
+    // const ip = request.headers.get('x-real-ip'); // 获取客户端IP地址
+    // const ip = request.headers.get('x-real-ip') ?? request.headers.get('x-forwarded-for')?.split(',')[0] ?? request.conn.remoteAddr.hostname;
+    const ip = RequestIp.getClientIp(request)
+    console.log("ip = " + ip);
     return {
       body: JSON.stringify({
         name: 'Astro',
         url: 'https://astro.build/',
+        ip: ip === undefined ? "" : ip, // 将IP地址添加到返回的JSON对象中
       }),
     };
   }
